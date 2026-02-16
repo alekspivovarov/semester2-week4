10e79055490da81c4b16eb1257a8f3fa4bb4e294
alekspivovarov <mkkr0724@leeds.ac.uk>
2026-02-16 21:27:35 +0000

    all tasks completed

--- session-1/task3/dot_product.c
+++ session-1/task3/dot_product.c
@@ -1,13 +1,14 @@
-
 /*
  * Dot product of 2 vectors
  */
 
  #include <stdio.h>
+ #include <stdlib.h> // this is for calloc() and free()
 
  int main( void ) {
     int n = 5;
     float *a, *b, *d;  // stack-based pointers
+    float dot_product = 0.0; // scalar, not a vector
 
     // allocation of heap memory for vectors
     a = calloc( n, sizeof(float) );
@@ -22,6 +23,20 @@
     Store your answer in d
     print your final answer
     */
+
+    //initialise the vectors with numerical data
+    for (int k = 0; k<n; k++) {
+      a[k] = k + 1.0; // a = [1, 2, 3, 4, 5]
+      b[k] = (k + 1) * 2.0; // b = [2, 4, 6, 8, 10]
+    }
+
+    // compute the dot product
+    for (int k = 0; k < n; k++) {
+      d[k] = a[k] * b[k]; // store each product in d[k]
+      dot_product += d[k]; // Sum up for final result
+    }
+
+    printf("Final dot product result: %.1f\n", dot_product);
 
     // explicit deallocation of heap memory before exit */
     free(a);
--- session-1/task3/matrix_vector.c
+++ session-1/task3/matrix_vector.c
@@ -4,19 +4,54 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( void ) {
     int n = 4;           
     float **a, *b, *c;
 
-    /*
-    Dynamically allocate the matrix a and vectors b and c using size n
-    Intialise the matrix a and vector b entries to 1. 
-    Write code to compute the product.
-    Store your answer in vector c
-    Print your final answer
-    Free the allocated memory
-    */
     
+    //Dynamically allocate the matrix a and vectors b and c using size n/
+    a = malloc(n * sizeof(float*));
+    for (int i = 0; i < n; i++) {
+        a[i] = malloc(n * sizeof(float));
+    }
+    b = malloc(n * sizeof(float));
+    c = malloc(n * sizeof(float));
+
+
+    //Intialise the matrix a and vector b entries to 1. 
+    for (int i = 0; i < n; i++) {
+        b[i] = 1.0;
+        for (int j = 0; j < n; j++) {
+            a[i][j] = 1.0;
+        }
+    }
+
+    //Write code to compute the product. Store your answer in vector c
+    for (int i=0; i < n; i++) {
+        c[i] = 0.0;
+        for (int j = 0; j < n; j++) {
+            c[i] += a[i][j] * b[j];
+        }
+    }
+
+    
+    //Print your final answer
+    printf("Result: "); 
+    for (int i = 0; i<n; i++) {
+        printf("%.1f", c[i]);
+    }  
+    printf("\n");
+
+    //Free the allocated memory
+    for (int i = 0; i < n; i++) {
+        free(a[i]);
+    }
+
+    free(a);
+    free(b);
+    free(c);
+
     return 0;
  }
