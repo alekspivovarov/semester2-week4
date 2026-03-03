bb366bf361a8c9d68fcdcd6547759ad2ec1f717d
alekspivovarov <mkkr0724@leeds.ac.uk>
2026-03-03 12:27:53 +0000

    worksheet completed

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Aleksandr Pivovarov
+Student ID: 202029245
 */
 
 #include <stdio.h>
@@ -10,10 +10,12 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[50];
 
     // process the command-line data using appropriate string functions
+    strcpy(buffer, argv[1]);
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+    printf("%s\n", buffer); // use only this single print statement in your submitted code
 
     return 0;
 }
